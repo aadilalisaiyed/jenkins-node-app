1e693016b2e642ecb159a8d2a09c296f8c6a3961
Aadil Saiyed <aadil.saiyed0327@gmail.com>
2026-02-26 12:12:54 +0530

    Update and rename package.json to app.js

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 {
   "name": "jenkins-demo",
   "version": "1.0.0",
+  "description": "Simple CI demo project",
+  "main": "app.js",
   "scripts": {
     "start": "node app.js",
     "test": "node app.js"
-  }
+  },
+  "author": "Aadil",
+  "license": "ISC"
 }
